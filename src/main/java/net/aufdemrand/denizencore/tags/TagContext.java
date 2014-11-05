6147686bf748aa95fce1518b8236575844d4056e
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-11-05 15:39:32 -0800

    Work towards moving TagManager to the core

---
+++ src/main/java/net/aufdemrand/denizencore/tags/TagContext.java
@@ -0,0 +1,11 @@
+package net.aufdemrand.denizencore.tags;
+
+public class TagContext {
+    public final boolean instant;
+    public final boolean debug;
+    public TagContext( boolean instant, boolean debug) {
+        this.instant = instant;
+        this.debug = debug;
+    }
+}
+
