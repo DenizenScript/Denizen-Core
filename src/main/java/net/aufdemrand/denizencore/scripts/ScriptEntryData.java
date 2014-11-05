9d200ddd0c65dd20386c3dc8ba48b461c85465d4
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-11-05 15:06:14 -0800

    Patch up ScriptEntryData.clone()

--- src/main/java/net/aufdemrand/denizencore/scripts/ScriptEntryData.java
+++ src/main/java/net/aufdemrand/denizencore/scripts/ScriptEntryData.java
@@ -1,12 +1,18 @@
 package net.aufdemrand.denizencore.scripts;
 
-import net.aufdemrand.denizencore.utilities.debugging.Debuggable;
+import net.aufdemrand.denizencore.utilities.debugging.dB;
 
 public abstract class ScriptEntryData implements Cloneable {
-    // TODO: private ScriptEntry entry;
+    // TODO: private ScriptEntry entry; ?
 
     @Override
-    public Object clone() throws CloneNotSupportedException {
-        return super.clone();
+    public ScriptEntryData clone() {
+        try {
+            return (ScriptEntryData)super.clone();
+        }
+        catch (Exception e) {
+            dB.echoError(e);
+            return null;
+        }
     }
 }
