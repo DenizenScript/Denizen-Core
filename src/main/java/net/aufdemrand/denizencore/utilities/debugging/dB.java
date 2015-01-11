5f98b26048807bfc3f5a49ca787171ce893911e4
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-01-10 19:17:47 -0800

    move DebugElement to the core

--- src/main/java/net/aufdemrand/denizencore/utilities/debugging/dB.java
+++ src/main/java/net/aufdemrand/denizencore/utilities/debugging/dB.java
@@ -4,6 +4,20 @@ import net.aufdemrand.denizencore.DenizenCore;
 
 public class dB {
 
+    /**
+     * Can be used with echoDebug(...) to output a header, footer,
+     * or a spacer.
+     *
+     * DebugElement.Header = +- string description ------+
+     * DebugElement.Spacer =
+     * DebugElement.Footer = +--------------+
+     *
+     * Also includes color.
+     */
+    public static enum DebugElement {
+        Header, Footer, Spacer
+    }
+
     public static void echoError(String error) {
         DenizenCore.getImplementation().debugError(error);
     }
