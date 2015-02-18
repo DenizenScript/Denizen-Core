a02d9d1a9ece8379afcbe5c8b8f4d987923bc5ec
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-02-18 14:14:25 -0800

    Catch null comparisons in the sort
    This should never be needed, but...

--- src/main/java/net/aufdemrand/denizencore/utilities/NaturalOrderComparator.java
+++ src/main/java/net/aufdemrand/denizencore/utilities/NaturalOrderComparator.java
@@ -67,6 +67,15 @@ public class NaturalOrderComparator implements Comparator
     }
     public int compare(Object o1, Object o2)
     {
+        if (o1 == null && o2 == null) {
+             return 0;
+        }
+        else if (o1 == null) {
+            return 1;
+        }
+        else if (o2 == null) {
+            return -1;
+        }
         String a = o1.toString();
         String b = o2.toString();
         int ia = 0, ib = 0;
