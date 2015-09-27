4fb43bbd42f41ebcec7f91e180a6b938135318f5
Fortifier42 <Fortifier@theausfort.net>
2015-09-27 13:30:24 +1000

    Add more elemen.contains tags

--- src/main/java/net/aufdemrand/denizencore/objects/Element.java
+++ src/main/java/net/aufdemrand/denizencore/objects/Element.java
@@ -723,6 +723,54 @@ public class Element implements dObject {
         r.name = null;
         registerTag("contains_text", r);
 
+        // <--[tag]
+        // @attribute <el@element.contains_all[<element>|...]>
+        // @returns Element(Boolean)
+        // @group element checking
+        // @description
+        // Returns whether the element contains all of the specified strings, case insensitive.
+        // -->
+        registerTag("contains_all", new TagRunnable() {
+            @Override
+            public String run(Attribute attribute, dObject object) {
+                String element = ((Element) object).element;
+                dList list = dList.valueOf(CoreUtilities.toLowerCase(attribute.getContext(1)));
+                String ellow = CoreUtilities.toLowerCase(element);
+                for (String list_element : list) {
+                    if (!ellow.contains(list_element)) {
+                        return new Element("false").getAttribute(attribute.fulfill(1));
+                    }
+                }
+                return new Element("true").getAttribute(attribute.fulfill(1));
+            }
+        });
+        r = registeredTags.get("contains_all").clone();
+        r.name = null;
+        registerTag("contains_all_text", r);
+
+        // <--[tag]
+        // @attribute <el@element.contains_all_case_sensitive[<element>|...]>
+        // @returns Element(Boolean)
+        // @group element checking
+        // @description
+        // Returns whether the element contains all of the specified strings, case sensitive.
+        // -->
+        registerTag("contains_all_case_sensitive", new TagRunnable() {
+            @Override
+            public String run(Attribute attribute, dObject object) {
+                String element = ((Element) object).element;
+                dList list = dList.valueOf(attribute.getContext(1));
+                for (String list_element : list) {
+                    if (!element.contains(list_element)) {
+                        return new Element("false").getAttribute(attribute.fulfill(1));
+                    }
+                }
+                return new Element("true").getAttribute(attribute.fulfill(1));
+            }
+        });
+        r = registeredTags.get("contains_all_case_sensitive").clone();
+        r.name = null;
+        registerTag("contains_all_case_sensitive_text", r);
 
         // <--[tag]
         // @attribute <el@element.ends_with[<element>]>
