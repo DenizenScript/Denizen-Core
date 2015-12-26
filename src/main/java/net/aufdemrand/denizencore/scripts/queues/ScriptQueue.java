ed3b35953041a9cf91d5624c1114b0ebb3f90979
Fortifier42 <fortifier@theausfort.net>
2015-12-26 15:32:09 +1100

    Add paused to <q@queue.state>

--- src/main/java/net/aufdemrand/denizencore/scripts/queues/ScriptQueue.java
+++ src/main/java/net/aufdemrand/denizencore/scripts/queues/ScriptQueue.java
@@ -8,6 +8,7 @@ import net.aufdemrand.denizencore.objects.properties.Property;
 import net.aufdemrand.denizencore.objects.properties.PropertyParser;
 import net.aufdemrand.denizencore.scripts.ScriptEntry;
 import net.aufdemrand.denizencore.scripts.commands.core.DetermineCommand;
+import net.aufdemrand.denizencore.scripts.queues.core.Delayable;
 import net.aufdemrand.denizencore.scripts.queues.core.TimedQueue;
 import net.aufdemrand.denizencore.tags.Attribute;
 import net.aufdemrand.denizencore.tags.TagContext;
@@ -864,13 +865,14 @@ public abstract class ScriptQueue implements Debuggable, dObject, DefinitionProv
         // @attribute <q@queue.state>
         // @returns Element
         // @description
-        // Returns 'stopping', 'running', or 'unknown'.
+        // Returns 'stopping', 'running', 'paused', or 'unknown'.
         // -->
         registerTag("state", new TagRunnable() {
             @Override
             public String run(Attribute attribute, dObject object) {
                 String state;
-                if (((ScriptQueue) object).is_started) state = "running";
+                if ((object instanceof Delayable) && ((Delayable) object).isPaused()) state = "paused";
+                else if (((ScriptQueue) object).is_started) state = "running";
                 else if (((ScriptQueue) object).is_stopping) state = "stopping";
                 else state = "unknown";
                 return new Element(state).getAttribute(attribute.fulfill(1));
