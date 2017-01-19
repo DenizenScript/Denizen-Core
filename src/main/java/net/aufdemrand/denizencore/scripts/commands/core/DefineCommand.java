21b2ce8763941a7c21d2ccf5ef350b0e9e816b56
Morphan1 <morphanone@gmail.com>
2017-01-19 15:54:33 -0500

    Add no_parse argument to define command

--- src/main/java/net/aufdemrand/denizencore/scripts/commands/core/DefineCommand.java
+++ src/main/java/net/aufdemrand/denizencore/scripts/commands/core/DefineCommand.java
@@ -1,11 +1,14 @@
 package net.aufdemrand.denizencore.scripts.commands.core;
 
+import net.aufdemrand.denizencore.DenizenCore;
 import net.aufdemrand.denizencore.exceptions.CommandExecutionException;
 import net.aufdemrand.denizencore.exceptions.InvalidArgumentsException;
 import net.aufdemrand.denizencore.objects.Element;
 import net.aufdemrand.denizencore.objects.aH;
 import net.aufdemrand.denizencore.scripts.ScriptEntry;
 import net.aufdemrand.denizencore.scripts.commands.AbstractCommand;
+import net.aufdemrand.denizencore.tags.TagContext;
+import net.aufdemrand.denizencore.tags.TagManager;
 import net.aufdemrand.denizencore.utilities.CoreUtilities;
 import net.aufdemrand.denizencore.utilities.debugging.dB;
 
@@ -15,25 +18,36 @@ import net.aufdemrand.denizencore.utilities.debugging.dB;
 public class DefineCommand extends AbstractCommand {
 
     @Override
+    public void onEnable() {
+        setParseArgs(false);
+    }
+
+    @Override
     public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
 
-        for (aH.Argument arg : aH.interpret(scriptEntry.getArguments())) {
+        TagContext context = DenizenCore.getImplementation().getTagContext(scriptEntry);
+
+        for (aH.Argument arg : aH.interpret(scriptEntry.getOriginalArguments())) {
 
             if (!scriptEntry.hasObject("definition")) {
                 if (arg.getValue().equals("!") && arg.hasPrefix()) {
                     scriptEntry.addObject("remove", new Element("true"));
                     scriptEntry.addObject("value", new Element("null"));
-                    scriptEntry.addObject("definition", arg.getPrefix().asElement());
+                    scriptEntry.addObject("definition", new Element(TagManager.tag(arg.getPrefix().getValue(), context)));
                 }
                 else {
-                    scriptEntry.addObject("definition", new Element(CoreUtilities.toLowerCase(arg.getValue())));
+                    scriptEntry.addObject("definition", new Element(CoreUtilities.toLowerCase(TagManager.tag(arg.getValue(), context))));
                 }
             }
 
-            else if (!scriptEntry.hasObject("value"))
-            // Use the raw_value as to not exclude values with :'s in them.
-            {
+            else if (!scriptEntry.hasObject("value")) {
+                // Use the raw_value as to not exclude values with :'s in them.
                 scriptEntry.addObject("value", new Element(arg.raw_value));
+            }
+
+            else if (!scriptEntry.hasObject("no_parse")
+                    && arg.matches("no_parse")) {
+                scriptEntry.addObject("no_parse", new Element(true));
             }
 
             else {
@@ -52,17 +66,25 @@ public class DefineCommand extends AbstractCommand {
         Element definition = scriptEntry.getElement("definition");
         Element value = scriptEntry.getElement("value");
         Element remove = scriptEntry.getElement("remove");
+        Element noParse = scriptEntry.getElement("no_parse");
+
+        String parsedValue = value.asString();
+
+        if (noParse == null) {
+            parsedValue = TagManager.tag(parsedValue, DenizenCore.getImplementation().getTagContext(scriptEntry));
+        }
 
         dB.report(scriptEntry, getName(), aH.debugObj("queue", scriptEntry.getResidingQueue().id)
                 + definition.debug()
-                + value.debug()
-                + (remove != null ? remove.debug() : ""));
+                + aH.debugObj("value", parsedValue)
+                + (remove != null ? remove.debug() : "")
+                + (noParse != null ? noParse.debug() : ""));
 
         if (scriptEntry.hasObject("remove")) {
             scriptEntry.getResidingQueue().removeDefinition(definition.asString());
         }
         else {
-            scriptEntry.getResidingQueue().addDefinition(definition.asString(), value.asString());
+            scriptEntry.getResidingQueue().addDefinition(definition.asString(), parsedValue);
         }
     }
 }
