91609fd1d422d26bedd5bb4c38df52d1b7c30e28
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-06-26 08:48:43 -0700

    move run command to the core

--- src/main/java/net/aufdemrand/denizencore/scripts/commands/CommandRegistry.java
+++ src/main/java/net/aufdemrand/denizencore/scripts/commands/CommandRegistry.java
@@ -107,6 +107,7 @@ public abstract class CommandRegistry implements dRegistry {
         registerCoreMember(RandomCommand.class, "RANDOM", "random [<#>/<commands>]", 0);
         registerCoreMember(ReloadCommand.class, "RELOAD", "reload", 0);
         registerCoreMember(RepeatCommand.class, "REPEAT", "repeat [stop/next/<amount>] [<commands>] (as:<name>)", 1);
+        registerCoreMember(RunCommand.class, "RUN", "run (locally) [<script>] (path:<name>) (def:<element>|...) (id:<name>) (instantly) (speed:<value>) (delay:<value>)", 1);
         registerCoreMember(SQLCommand.class, "SQL", "sql [id:<ID>] [disconnect/connect:<server> (username:<username>) (password:<password>) (ssl:true/{false})/query:<query>/update:<update>]", 2);
         registerCoreMember(SyncCommand.class, "SYNC", "sync [<commands>]", 0);
         registerCoreMember(WaitCommand.class, "WAIT", "wait (<duration>) (queue:<name>)", 0);
