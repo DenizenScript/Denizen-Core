b8967a6584e859168880052c08b89b4f57f59726
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-09-15 08:24:01 -0700

    add better support for objectAttribute in property

--- src/main/java/com/denizenscript/denizencore/objects/properties/Property.java
+++ src/main/java/com/denizenscript/denizencore/objects/properties/Property.java
@@ -4,6 +4,7 @@ import com.denizenscript.denizencore.objects.Mechanism;
 import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.tags.Attribute;
+import com.denizenscript.denizencore.utilities.CoreUtilities;
 
 public interface Property {
 
@@ -16,7 +17,9 @@ public interface Property {
         return res == null ? null : new ElementTag(res);
     }
 
-    String getAttribute(Attribute attribute);
+    default String getAttribute(Attribute attribute) {
+        return CoreUtilities.stringifyNullPass(getObjectAttribute(attribute));
+    }
 
     void adjust(Mechanism mechanism);
 }
