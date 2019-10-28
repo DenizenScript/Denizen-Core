121fadac11e5f9266dfc17da5b579ea7e93bde6c
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-10-28 13:23:17 -0700

    cleaner/more proper SQL writeup
    better asynciness

--- src/main/java/com/denizenscript/denizencore/scripts/commands/core/SQLCommand.java
+++ src/main/java/com/denizenscript/denizencore/scripts/commands/core/SQLCommand.java
@@ -38,8 +38,8 @@ public class SQLCommand extends AbstractCommand implements Holdable {
     // The SQL command is merely a wrapper for SQL queries, and further usage details should be gathered from an official
     // MySQL query reference rather than from Denizen command help.
     // SQL connections are not instant - they can take several seconds, or just never connect at all.
-    // It is recommended you hold the connection command by doing "- ~sql ..." rather than just "- sql ..."
-    // as this will delay the commands following the connect command until after the connection is established.
+    // It is recommended you hold the command by doing "- ~sql ..." rather than just "- sql ..."
+    // as this will delay the commands following the SQL command until after the SQL operation is complete.
     //
     // @Tags
     // <entry[saveName].result> returns a ListTag of all rows from a query or update command, of the form li@escaped_text/escaped_text|escaped_text/escaped_text
@@ -59,20 +59,20 @@ public class SQLCommand extends AbstractCommand implements Holdable {
     //
     // @Usage
     // Use to update an SQL server.
-    // - sql id:name "update:CREATE table things(id int,column_name1 varchar(255),column_name2 varchar(255));"
+    // - ~sql id:name "update:CREATE table things(id int,column_name1 varchar(255),column_name2 varchar(255));"
     //
     // @Usage
     // Use to update an SQL server.
-    // - sql id:name "update:INSERT INTO things VALUES (3, 'hello', 'space');"
+    // - ~sql id:name "update:INSERT INTO things VALUES (3, 'hello', 'space');"
     //
     // @Usage
     // Use to query an SQL server.
-    // - sql id:name "query:SELECT id,column_name1,column_name2 FROM things;" save:saveName
+    // - ~sql id:name "query:SELECT id,column_name1,column_name2 FROM things;" save:saveName
     // - narrate <entry[saveName].result>
     //
     // @Usage
     // Use to query an SQL server.
-    // - sql id:name "query:SELECT id,column_name1,column_name2 FROM things WHERE id=3;" save:saveName2
+    // - ~sql id:name "query:SELECT id,column_name1,column_name2 FROM things WHERE id=3;" save:saveName2
     // - narrate <entry[saveName2].result>
     //
     // @Usage
@@ -270,75 +270,47 @@ public class SQLCommand extends AbstractCommand implements Holdable {
                     return;
                 }
                 Debug.echoDebug(scriptEntry, "Running query " + query.asString());
-                if (scriptEntry.shouldWaitFor()) {
-                    DenizenCore.schedule(new AsyncSchedulable(new OneTimeSchedulable(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                Statement statement = con.createStatement();
-                                ResultSet set = statement.executeQuery(query.asString());
-                                ResultSetMetaData rsmd = set.getMetaData();
-                                final int columns = rsmd.getColumnCount();
-                                DenizenCore.schedule(new OneTimeSchedulable(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Debug.echoDebug(scriptEntry, "Got a query result of " + columns + " columns");
-                                    }
-                                }, 0));
-                                int count = 0;
-                                ListTag rows = new ListTag();
-                                while (set.next()) {
-                                    count++;
-                                    StringBuilder current = new StringBuilder();
-                                    for (int i = 0; i < columns; i++) {
-                                        current.append(EscapeTagBase.escape(set.getString(i + 1))).append("/");
-                                    }
-                                    rows.add(current.toString());
-                                }
-                                scriptEntry.addObject("result", rows);
-                                final int finalCount = count;
-                                DenizenCore.schedule(new OneTimeSchedulable(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Debug.echoDebug(scriptEntry, "Got a query result of " + finalCount + " rows");
-                                        scriptEntry.setFinished(true);
-                                    }
-                                }, 0));
+                Runnable doQuery = () -> {
+                    try {
+                        Statement statement = con.createStatement();
+                        ResultSet set = statement.executeQuery(query.asString());
+                        ResultSetMetaData rsmd = set.getMetaData();
+                        final int columns = rsmd.getColumnCount();
+                        int count = 0;
+                        ListTag rows = new ListTag();
+                        while (set.next()) {
+                            count++;
+                            StringBuilder current = new StringBuilder();
+                            for (int i = 0; i < columns; i++) {
+                                current.append(EscapeTagBase.escape(set.getString(i + 1))).append("/");
                             }
-                            catch (final Exception e) {
-                                DenizenCore.schedule(new OneTimeSchedulable(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Debug.echoError(scriptEntry.getResidingQueue(), "SQL Exception: " + e.getMessage());
-                                        scriptEntry.setFinished(true);
-                                        if (Debug.verbose) {
-                                            Debug.echoError(scriptEntry.getResidingQueue(), e);
-                                        }
-                                    }
-                                }, 0));
-                            }
+                            rows.add(current.toString());
                         }
-                    }, 0)));
+                        scriptEntry.addObject("result", rows);
+                        final int finalCount = count;
+                        DenizenCore.schedule(new OneTimeSchedulable(new Runnable() {
+                            @Override
+                            public void run() {
+                                Debug.echoDebug(scriptEntry, "Got a query result of " + columns + " columns and " + finalCount + " rows");
+                                scriptEntry.setFinished(true);
+                            }
+                        }, 0));
+                    }
+                    catch (final Exception ex) {
+                        DenizenCore.schedule(new OneTimeSchedulable(() -> {
+                            Debug.echoError(scriptEntry.getResidingQueue(), "SQL Exception: " + ex.getMessage());
+                            scriptEntry.setFinished(true);
+                            if (Debug.verbose) {
+                                Debug.echoError(scriptEntry.getResidingQueue(), ex);
+                            }
+                        }, 0));
+                    }
+                };
+                if (scriptEntry.shouldWaitFor()) {
+                    DenizenCore.schedule(new AsyncSchedulable(new OneTimeSchedulable(doQuery, 0)));
                 }
                 else {
-                    Statement statement = con.createStatement();
-                    ResultSet set = statement.executeQuery(query.asString());
-                    ResultSetMetaData rsmd = set.getMetaData();
-                    final int columns = rsmd.getColumnCount();
-                    Debug.echoDebug(scriptEntry, "Got a query result of " + columns + " columns");
-                    int count = 0;
-                    ListTag rows = new ListTag();
-                    while (set.next()) {
-                        count++;
-                        StringBuilder current = new StringBuilder();
-                        for (int i = 0; i < columns; i++) {
-                            current.append(EscapeTagBase.escape(set.getString(i + 1))).append("/");
-                        }
-                        rows.add(current.toString());
-                    }
-                    scriptEntry.addObject("result", rows);
-                    final int finalCount = count;
-                    Debug.echoDebug(scriptEntry, "Got a query result of " + finalCount + " rows");
+                    doQuery.run();
                 }
             }
             else if (action.asString().equalsIgnoreCase("update")) {
@@ -346,38 +318,59 @@ public class SQLCommand extends AbstractCommand implements Holdable {
                     Debug.echoError(scriptEntry.getResidingQueue(), "Must specify an update query!");
                     return;
                 }
-                Connection con = connections.get(sqlID.asString().toUpperCase());
+                final Connection con = connections.get(sqlID.asString().toUpperCase());
                 if (con == null) {
                     Debug.echoError(scriptEntry.getResidingQueue(), "Not connected to server with ID '" + sqlID.asString() + "'!");
                     return;
                 }
                 Debug.echoDebug(scriptEntry, "Running update " + query.asString());
-                Statement statement = con.createStatement();
-                int affected = statement.executeUpdate(query.asString(), Statement.RETURN_GENERATED_KEYS);
-                scriptEntry.addObject("affected_rows", new ElementTag(affected));
-                ResultSet set = statement.getGeneratedKeys();
-                ResultSetMetaData rsmd = set.getMetaData();
-                int columns = rsmd.getColumnCount();
-                Debug.echoDebug(scriptEntry, "Got a query result of " + columns + " columns");
-                ListTag rows = new ListTag();
-                while (set.next()) {
-                    StringBuilder current = new StringBuilder();
-                    for (int i = 0; i < columns; i++) {
-                        current.append(EscapeTagBase.escape(set.getString(i + 1))).append("/");
+                Runnable doUpdate = () -> {
+                    try {
+                        Statement statement = con.createStatement();
+                        int affected = statement.executeUpdate(query.asString(), Statement.RETURN_GENERATED_KEYS);
+                        scriptEntry.addObject("affected_rows", new ElementTag(affected));
+                        ResultSet set = statement.getGeneratedKeys();
+                        ResultSetMetaData rsmd = set.getMetaData();
+                        int columns = rsmd.getColumnCount();
+                        ListTag rows = new ListTag();
+                        while (set.next()) {
+                            StringBuilder current = new StringBuilder();
+                            for (int i = 0; i < columns; i++) {
+                                current.append(EscapeTagBase.escape(set.getString(i + 1))).append("/");
+                            }
+                            rows.add(current.toString());
+                        }
+                        scriptEntry.addObject("result", rows);
+                        DenizenCore.schedule(new OneTimeSchedulable(() -> {
+                            Debug.echoDebug(scriptEntry, "Got a query result of " + columns + " columns");
+                            Debug.echoDebug(scriptEntry, "Updated " + affected + " rows");
+                            scriptEntry.setFinished(true);
+                        }, 0));
                     }
-                    rows.add(current.toString());
+                    catch (Exception ex) {
+                        DenizenCore.schedule(new OneTimeSchedulable(() -> {
+                            Debug.echoError(scriptEntry.getResidingQueue(), "SQL Exception: " + ex.getMessage());
+                            if (Debug.verbose) {
+                                Debug.echoError(scriptEntry.getResidingQueue(), ex);
+                            }
+                        }, 0));
+                    }
+                };
+                if (scriptEntry.shouldWaitFor()) {
+                    DenizenCore.schedule(new AsyncSchedulable(new OneTimeSchedulable(doUpdate, 0)));
                 }
-                scriptEntry.addObject("result", rows);
-                Debug.echoDebug(scriptEntry, "Updated " + affected + " rows");
+                else {
+                    doUpdate.run();
+                }
             }
             else {
                 Debug.echoError(scriptEntry.getResidingQueue(), "Unknown action '" + action.asString() + "'");
             }
         }
-        catch (SQLException e) {
-            Debug.echoError(scriptEntry.getResidingQueue(), "SQL Exception: " + e.getMessage());
+        catch (SQLException ex) {
+            Debug.echoError(scriptEntry.getResidingQueue(), "SQL Exception: " + ex.getMessage());
             if (Debug.verbose) {
-                Debug.echoError(scriptEntry.getResidingQueue(), e);
+                Debug.echoError(scriptEntry.getResidingQueue(), ex);
             }
         }
     }
