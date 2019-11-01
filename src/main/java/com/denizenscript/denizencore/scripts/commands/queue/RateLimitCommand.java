3c8412bf0e6f9fa2516b1bebb946e1b69f6f85bc
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-11-01 14:57:19 -0700

    synchronize ratelimit with server lag

--- src/main/java/com/denizenscript/denizencore/scripts/commands/queue/RateLimitCommand.java
+++ src/main/java/com/denizenscript/denizencore/scripts/commands/queue/RateLimitCommand.java
@@ -1,5 +1,6 @@
 package com.denizenscript.denizencore.scripts.commands.queue;
 
+import com.denizenscript.denizencore.DenizenCore;
 import com.denizenscript.denizencore.exceptions.InvalidArgumentsException;
 import com.denizenscript.denizencore.objects.Argument;
 import com.denizenscript.denizencore.objects.core.DurationTag;
@@ -72,7 +73,7 @@ public class RateLimitCommand extends AbstractCommand {
         HashMap<String, Long> map = (HashMap<String, Long>) scriptEntry.internal.specialProcessedData;
         String key = CoreUtilities.toLowerCase(object.asString());
         Long endTime = map.get(key);
-        long curTime = System.currentTimeMillis();
+        long curTime = DenizenCore.serverTimeMillis;
         if (endTime != null && curTime < endTime) {
             Debug.echoDebug(scriptEntry, "Rate limit applied with " + (endTime - curTime) + "ms left.");
             scriptEntry.getResidingQueue().clear();
