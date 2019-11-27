433e54b5b70fa79c14bb3ef278865d8239de8a7d
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-11-27 08:57:37 -0800

    slight cleanup to choose command

--- src/main/java/com/denizenscript/denizencore/scripts/commands/queue/ChooseCommand.java
+++ src/main/java/com/denizenscript/denizencore/scripts/commands/queue/ChooseCommand.java
@@ -29,34 +29,35 @@ public class ChooseCommand extends BracedCommand {
     // Optionally, specify "default" in place of a case to give a result when all other cases fail to match.
     //
     // Cases must be static text. They may not contain tags. For multi-tag comparison, consider the IF command.
+    // Any one case line can have multiple values in it - each possible value should be its own argument (separated by spaces).
     //
     // @Tags
     // None
     //
     // @Usage
     // Use to choose the only case.
-    // - choose "1":
-    //   - case "1":
+    // - choose 1:
+    //   - case 1:
     //     - debug LOG "Success!"
     //
     // @Usage
     // Use to choose the default case.
-    // - choose "2":
-    //   - case "1":
+    // - choose 2:
+    //   - case 1:
     //     - debug log "Failure!"
     //   - default:
     //     - debug log "Success!"
     //
     // @Usage
     // Use for dynamically choosing a case.
-    // - choose "<[entity_type]>":
-    //   - case "zombie":
+    // - choose <[entity_type]>:
+    //   - case zombie:
     //     - narrate "You slayed an undead zombie!"
-    //   - case "skeleton":
+    //   - case skeleton:
     //     - narrate "You knocked the bones out of a skeleton!"
-    //   - case "creeper":
+    //   - case creeper:
     //     - narrate "You didn't give that creeper a chance to explode!"
-    //   - case "pig" "cow" "chicken":
+    //   - case pig cow chicken:
     //     - narrate "You killed an innocent farm animal!"
     //   - default:
     //     - narrate "You killed a <[entity_type].to_titlecase>!"
@@ -70,21 +71,16 @@ public class ChooseCommand extends BracedCommand {
 
     @Override
     public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
-
         for (Argument arg : scriptEntry.getProcessedArgs()) {
-
             if (!scriptEntry.hasObject("choice")) {
                 scriptEntry.addObject("choice", arg.asElement());
-                scriptEntry.addObject("braces", getBracedCommands(scriptEntry, false));
                 break;
             }
-
             else {
                 arg.reportUnhandled();
                 break;
             }
         }
-
         if (!scriptEntry.hasObject("choice")) {
             throw new InvalidArgumentsException("Must have a choice!"); // Should never happen
         }
@@ -93,15 +89,13 @@ public class ChooseCommand extends BracedCommand {
 
     @Override
     public void execute(ScriptEntry scriptEntry) {
-
-        List<BracedData> bdlist = (List<BracedData>) scriptEntry.getObject("braces");
+        List<BracedData> bdlist = getBracedCommands(scriptEntry, false);
         if (bdlist == null || bdlist.isEmpty()) {
             Debug.echoError(scriptEntry.getResidingQueue(), "Empty sub-commands (internal)!");
             return;
         }
         List<ScriptEntry> bracedCommandsList = bdlist.get(0).value;
-
-        HashMap<String, Integer> lookupTable = null;
+        HashMap<String, Integer> lookupTable;
         if (scriptEntry.internal.specialProcessedData instanceof HashMap) {
             lookupTable = (HashMap<String, Integer>) scriptEntry.internal.specialProcessedData;
         }
@@ -130,17 +124,12 @@ public class ChooseCommand extends BracedCommand {
             }
             scriptEntry.internal.specialProcessedData = lookupTable;
         }
-
         ElementTag choice = scriptEntry.getElement("choice");
-
         if (scriptEntry.dbCallShouldDebug()) {
             Debug.report(scriptEntry, getName(), choice.debug());
         }
-
         String choice_low = CoreUtilities.toLowerCase(choice.asString());
-
         Integer resultIndex = lookupTable.get(choice_low);
-
         if (resultIndex == null) {
             resultIndex = lookupTable.get("\0DEFAULT");
             if (resultIndex == null) {
@@ -148,26 +137,19 @@ public class ChooseCommand extends BracedCommand {
                 return;
             }
         }
-
         ScriptEntry result = bracedCommandsList.get(resultIndex);
-
         List<BracedData> new_commands = getBracedCommands(result);
-
         if (new_commands == null || new_commands.isEmpty()) {
             Debug.echoError(scriptEntry.getResidingQueue(), "Empty choose command case sub-commands (internal) for case '" + result.toString() + "'");
             return;
         }
-
         List<ScriptEntry> new_command_list = new_commands.get(0).value;
-
         for (ScriptEntry newEntry : new_command_list) {
             newEntry.setInstant(true);
             newEntry.entryData.transferDataFrom(scriptEntry.entryData);
             newEntry.entryData.scriptEntry = newEntry;
         }
-
         scriptEntry.setInstant(true);
-
         scriptEntry.getResidingQueue().injectEntries(new_command_list, 0);
     }
 }
