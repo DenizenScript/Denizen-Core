720369b4c3a2ce548dd52b6f2d337004baa136e4
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-12-14 19:43:58 -0800

    move generic event check logic to core
    also add a null check

--- src/main/java/com/denizenscript/denizencore/events/ScriptEvent.java
+++ src/main/java/com/denizenscript/denizencore/events/ScriptEvent.java
@@ -587,4 +587,38 @@ public abstract class ScriptEvent implements ContextSource, Cloneable {
         input = CoreUtilities.toLowerCase(input);
         return input.equals(compared) || (regexed != null && regexed.matcher(input).matches());
     }
+
+    public boolean runGenericCheck(String inputValue, String trueValue) {
+        if (inputValue != null) {
+            trueValue = CoreUtilities.toLowerCase(trueValue);
+            inputValue = CoreUtilities.toLowerCase(inputValue);
+            if (inputValue.equals(trueValue)) {
+                return true;
+            }
+            Pattern regexd = regexHandle(inputValue);
+            if (!equalityCheck(trueValue, inputValue, regexd)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean runGenericSwitchCheck(ScriptPath path, String switchName, String value) {
+        String with = path.switches.get(switchName);
+        if (with != null) {
+            if (value == null) {
+                return false;
+            }
+            value = CoreUtilities.toLowerCase(value);
+            with = CoreUtilities.toLowerCase(with);
+            if (with.equals(value)) {
+                return true;
+            }
+            Pattern regexd = regexHandle(with);
+            if (!equalityCheck(value, with, regexd)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
