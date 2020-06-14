2a4f0c44d4e458536c0119cd30e960550a410909
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-06-13 21:10:04 -0700

    add future tag deprecation tool

--- src/main/java/com/denizenscript/denizencore/tags/ObjectTagProcessor.java
+++ src/main/java/com/denizenscript/denizencore/tags/ObjectTagProcessor.java
@@ -3,12 +3,27 @@ package com.denizenscript.denizencore.tags;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.utilities.CoreUtilities;
 import com.denizenscript.denizencore.utilities.debugging.Debug;
+import com.denizenscript.denizencore.utilities.debugging.FutureWarning;
 
 import java.util.HashMap;
 
 public class ObjectTagProcessor<T extends ObjectTag> {
 
     public HashMap<String, TagRunnable.ObjectInterface<T>> registeredObjectTags = new HashMap<>();
+
+    public void registerFutureTagDeprecation(String name, String... deprecatedVariants) {
+        TagRunnable.ObjectInterface<T> properTag = registeredObjectTags.get(name);
+        for (String variant : deprecatedVariants) {
+            TagRunnable.ObjectInterface<T> newRunnable = (attribute, object) -> {
+                if (FutureWarning.futureWarningsEnabled) {
+                    Debug.echoError(attribute.getScriptEntry() != null ? attribute.getScriptEntry().getResidingQueue() : null,
+                            "Using deprecated form of tag '" + name + "': '" + variant + "'.");
+                }
+                return properTag.run(attribute, object);
+            };
+            registeredObjectTags.put(variant, newRunnable);
+        }
+    }
 
     public void registerTag(String name, TagRunnable.ObjectInterface<T> runnable, String... deprecatedVariants) {
         for (String variant : deprecatedVariants) {
