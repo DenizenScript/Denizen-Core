edcd6307e5fc22a901c6b2565806a56acb9b3f1f
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2021-01-08 10:22:46 -0800

    add ContextSource.SimpleMap

--- src/main/java/com/denizenscript/denizencore/scripts/queues/ContextSource.java
+++ src/main/java/com/denizenscript/denizencore/scripts/queues/ContextSource.java
@@ -2,10 +2,22 @@ package com.denizenscript.denizencore.scripts.queues;
 
 import com.denizenscript.denizencore.objects.ObjectTag;
 
+import java.util.Map;
+
 /**
  * Provides contexts to a queue.
  */
 public interface ContextSource {
 
     ObjectTag getContext(String name);
+
+    class SimpleMap implements ContextSource {
+
+        public Map<String, ObjectTag> contexts;
+
+        @Override
+        public ObjectTag getContext(String name) {
+            return contexts.get(name);
+        }
+    }
 }
