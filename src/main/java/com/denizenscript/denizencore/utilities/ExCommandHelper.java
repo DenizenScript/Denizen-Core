7358ec5ca76de097b9c0f79909fb1a764645602d
Aya <31237389+tal5@users.noreply.github.com>
2023-04-03 20:23:40 +0000

    Fix flag tab complete

--- src/main/java/com/denizenscript/denizencore/utilities/ExCommandHelper.java
+++ src/main/java/com/denizenscript/denizencore/utilities/ExCommandHelper.java
@@ -221,9 +221,11 @@ public class ExCommandHelper {
                         }
                         else {
                             String tagPiece = subComponent.substring(0, squareBracket);
+                            String argUntilParam = arg.substring(0, relevantTagStart) + fullTag.substring(0, lastDot + squareBracket + 1);
                             if (tagPiece.startsWith("flag") || tagPiece.equals("has_flag")) {
                                 completionsBuilder.arg = subComponent.substring(squareBracket + 1);
                                 FlagCommand.tabCompleteFlag(completionsBuilder);
+                                completionsBuilder.completions.replaceAll(argUntilParam::concat);
                                 return completionsBuilder.completions;
                             }
                         }
