4ce4f8cc132facf82d738d220b2fd8fb01973f88
Aya <31237389+tal5@users.noreply.github.com>
2023-04-03 21:49:44 +0000

    Only create `argUntilParam` when needed

--- src/main/java/com/denizenscript/denizencore/utilities/ExCommandHelper.java
+++ src/main/java/com/denizenscript/denizencore/utilities/ExCommandHelper.java
@@ -221,10 +221,10 @@ public class ExCommandHelper {
                         }
                         else {
                             String tagPiece = subComponent.substring(0, squareBracket);
-                            String argUntilParam = arg.substring(0, relevantTagStart) + fullTag.substring(0, lastDot + squareBracket + 1);
                             if (tagPiece.startsWith("flag") || tagPiece.equals("has_flag")) {
                                 completionsBuilder.arg = subComponent.substring(squareBracket + 1);
                                 FlagCommand.tabCompleteFlag(completionsBuilder);
+                                String argUntilParam = arg.substring(0, relevantTagStart) + fullTag.substring(0, lastDot + squareBracket + 1);
                                 completionsBuilder.completions.replaceAll(argUntilParam::concat);
                                 return completionsBuilder.completions;
                             }
